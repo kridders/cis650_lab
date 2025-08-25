e785e12a38d10d5bf9231c56ccdedf1db145dc36
unknown <kridders@murraystate.edu>
2025-08-25 18:40:17 +0200

    Done with task 5

---
+++ Lab1_Statements/task5.py
@@ -0,0 +1,10 @@
+first_number = int(input("Please enter the first number: "))
+second_number = int(input("Please enter the second number: "))
+
+if first_number > second_number:
+    print (f"{first_number} is bigger than {second_number}.")
+elif first_number == second_number:
+    print(f"{first_number} is equal to {second_number}")
+elif first_number < second_number:
+    print (f"{first_number} is smaller than {second_number}")
+
