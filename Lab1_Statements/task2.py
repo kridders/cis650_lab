3ed071492a659ebf6ce6b8ae27c9a81d6b476aa7
unknown <kridders@murraystate.edu>
2025-08-25 18:02:31 +0200

    Done with task 2

---
+++ Lab1_Statements/task2.py
@@ -0,0 +1,4 @@
+miles_driven = float(input("Please enter the amount of miles driven: "))
+gallons_used = float(input("Please enter the amount of gallons used: "))
+
+print("Miles per gallon:", miles_driven/gallons_used)
