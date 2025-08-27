5abed380f3d4040a7faa116bd582d947a8003ba7
unknown <kridders@murraystate.edu>
2025-08-27 18:26:32 +0200

    Correction of task 5

--- Lab1_Statements/task5.py
+++ Lab1_Statements/task5.py
@@ -1,5 +1,16 @@
-first_number = int(input("Please enter the first number: "))
-second_number = int(input("Please enter the second number: "))
+first_number = input("Please enter the first number: ")
+while not first_number.isdigit():
+    print("You did not enter a valid number for the first number")
+    first_number = input("Please enter the first number: ")
+
+first_number = int(first_number)
+
+second_number = input("Please enter the second number: ")
+while not second_number.isdigit():
+    print("You did not enter a valid number for the second number")
+    second_number = input("Please enter the second number: ")
+
+second_number = int(second_number)
 
 if first_number > second_number:
     print (f"{first_number} is bigger than {second_number}.")
